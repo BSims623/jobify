dd578d18868ce07b938dcf8184cd1dfd02861e5c
Brian Sims <briansims623@gmail.com>
2023-11-26 18:50:45 -0800

    Initial Commit

---
+++ client/src/pages/DeleteJob.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import customFetch from '../utils/customFetch'
+import { redirect } from 'react-router-dom'
+import { toast } from 'react-toastify'
+
+export const action = async ({ params }) => {
+    try {
+        await customFetch.delete(`/jobs/${params.id}`)
+        toast.success('Job deleted successfully')
+    } catch (error) {
+        toast.error(error?.response?.data?.msg)
+    }
+    return redirect('../all-jobs')
+}
+
+const DeleteJob = () => {
+    return (
+        <h1>DeleteJob Page</h1>
+    )
+}
+
+export default DeleteJob
---
+++ client/src/pages/Landing.jsx
@@ -0,0 +1,36 @@
+import styled from 'styled-components'
+import React from 'react'
+import Wrapper from '../assets/wrappers/LandingPage'
+import main from '../assets/images/main.svg'
+import { Link } from 'react-router-dom'
+import { Logo } from '../components'
+
+
+const Landing = () => {
+    return (
+        <Wrapper>
+            <nav>
+                <Logo />
+            </nav>
+            <div className='container page'>
+                <div className="info">
+                    <h1>job <span>tracking</span> app</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Est assumenda dolore reprehenderit dolores laudantium, quibusdam dolorem. Facilis eius similique sequi, et voluptatum quisquam sed a expedita adipisci aut repellendus quos!
+                    </p>
+                    <Link to='/register' className='btn register-link'>
+                        Register
+                    </Link>
+                    <Link to='/login' className='btn'>
+                        Login / Demo User
+                    </Link>
+                </div>
+                <img src={main} alt="job hunt" className='img main-img' />
+            </div>
+        </Wrapper>
+
+    )
+}
+
+
+export default Landing
